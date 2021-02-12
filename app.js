d56376a5cc10f312dd3cf98cc89d44a2197e70ba
Gweltaz Mary <gweltazmary@gmail.com>
2021-02-12 17:09:44 +0100

    Basic directories and initial routing path

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const dotenv = require('dotenv')
 // loading config variables
 dotenv.config({ path: './config/config.env'})
 
+// Starting the server
 const app = express();
+
+// Enabling static files
+app.use(express.static('puclic'))
+
+// Server-side templating engine
+app.set('view engine', 'ejs')
 
 const PORT = process.env.PORT || 5000
 
@@ -13,6 +20,15 @@ app.listen(
   console.log(`Server running on port : ${PORT}`)
 )
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+// Setting Routes
+// Homepage
+const homepagePath = require('./routes/homepage')
+app.use('/', homepagePath)
+
+// User-Auth
+const userAuthPath = require('./routes/user-auth')
+app.use('user-auth', userAuthPath)
+
+// Contact
+const contactPath = require('./routes/contact')
+app.use('/contact', contactPath)
