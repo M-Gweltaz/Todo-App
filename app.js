45236d55f4a6e163fc1f1141cf28fb1fb41ff5ed
Gweltaz Mary <gweltazmary@gmail.com>
2021-02-12 16:20:59 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+// loading config variables
+dotenv.config({ path: './config/config.env'})
+
+const app = express();
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`Server running on port : ${PORT}`)
+)
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
